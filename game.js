4b6f8ef0f3e41b62ffb539a81f1a135b7c20c348
Brendan Burns <btburns10@gmail.com>
2018-07-13 20:44:03 -0400

    update data

--- game.js
+++ game.js
@@ -4,8 +4,9 @@ var moods = ["happy", "melancholy", "angry", "ecstatic", "sad", "joyful", "confu
 //giphy api key id
 var keyID = "vB7bIhdeXTpMRzEBcPPZROeDTHQGTBGE"
 
-//displays 10 gif images of selected button
+//displays 10 gif images from selected button
 function displayGifs() {
+    $("#gif-display").empty();
     var mood = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=" + keyID + "&limit=10";
 
@@ -15,16 +16,21 @@ function displayGifs() {
     }).then(function(response) {
         console.log(queryURL);
         response.data.forEach(function(data) {
-            var gifDiv = $("<div>");            
-            var rating = $("<p>");           
-            var gif = $("<img>");
-            gifDiv.append(rating, gif).addClass("gif-container");
+            const gifDiv = $("<div>");            
+            const rating = $("<p>");           
+            const gif = $("<img>");
+            const starDiv = $("<div>");
+            const star = $("<i>");
+            gifDiv.append(rating, gif, starDiv)
+                  .addClass("gif-container")
+                  .data({still: data.images.fixed_height_still.url,
+                    animate: data.images.fixed_height.url,
+                    state: "still",
+                    isFavorite: false});
             rating.text("Rating: " + data.rating);
             gif.attr("src", data.images.fixed_height_still.url)
-               .attr("data-still", data.images.fixed_height_still.url)
-               .attr("data-animate", data.images.fixed_height.url)
-               .attr("data-state", "still")
-               .addClass("gif");    
+            //    .addClass("gif");
+            starDiv.append(star).addClass("far fa-star star");
             $("#gif-display").append(gifDiv);
         })
 
@@ -34,6 +40,7 @@ function displayGifs() {
 
 function renderButtons() {
     $("#buttons").empty();
+    $("#mood-input").val("");
     moods.forEach(function(mood) {
         const moodBtn = $("<button>")
             .text(mood)
@@ -46,7 +53,7 @@ function renderButtons() {
 //fire function on browser load to render default buttons
 renderButtons();
 
-//create event handler to run function displayGifs()
+//event handler to run function displayGifs() when button is clicked
 $(document).on("click", ".mood", displayGifs);
 
 $("#add-mood").on("click", function() {
@@ -57,25 +64,64 @@ $("#add-mood").on("click", function() {
     // Here we grab the text from the input box
     var mood = $("#mood-input").val().trim();
 
-    moods.push(mood);
-
-    renderButtons();
+        if(mood === "") {
+            return;
+        }
+        else {
+            moods.push(mood);
+            renderButtons();
+        }
 
 });
 
-$(document).on("click", ".gif", function() {
-    var state = $(this).attr("data-state");
+//event handler to toggle gif animation
+$(document).on("click", ".gif-container img", function() {
+    const state = $(this).parent().data("state");
+    const animate = $(this).parent().data("animate");
+    const still = $(this).parent().data("still");
+
 
     if(state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
+        $(this).attr("src", animate);
+        $(this).parent().data("state", "animate");
+
     }
     else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
+        $(this).attr("src", still);
+        $(this).parent().data("state", "still");
     }
    
 })
+
+//event handler to determine isFavorite
+$(document).on("click", ".star", function() {
+    console.log($(this).parent());
+    
+    var isFav = $(this).attr("isFavorite");
+
+    if(isFav === "false") {
+        console.log("it worked");
+        $(this).attr("isFavorite", "true");
+        $(this).addClass("fas")
+    }
+    else {
+        $(this).attr("isFavorite", "false");
+        $(this).removeClass("fas");
+        console.log("true it is");
+    }
+    
+    
+    // toggleClass("far fas");
+
+
+})
+
+function displayFavGifs() {
+    //create for loop to iterate through fav gifs array
+    //create image for each index of array
+    //set attr for each image
+    //append each image to div with id #favorites-div
+}
 
 //create event handler for gifs so that they are appended to div with id #favorites-div
 // $(document).on("click", ".gif-container", function() {
